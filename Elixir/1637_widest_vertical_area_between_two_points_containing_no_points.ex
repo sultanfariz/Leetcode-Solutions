a5631efcc1d763af980634c3d3078f11c49c9367
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2024-02-08 22:57:30 +0700

    Update 1637_widest_vertical_area_between_two_points_containing_no_points.ex

--- Elixir/1637_widest_vertical_area_between_two_points_containing_no_points.ex
+++ Elixir/1637_widest_vertical_area_between_two_points_containing_no_points.ex
@@ -1,16 +1,16 @@
 defmodule Solution do
+  @spec calculate_width([integer]) :: [integer]
+  def calculate_width([]), do: []
+  def calculate_width([a, b]), do: [b-a]
+  def calculate_width([a, b | t]) do
+    [b-a] ++ calculate_width([b | t])
+  end
+
   @spec max_width_of_vertical_area(points :: [[integer]]) :: integer
   def max_width_of_vertical_area(points) do
     xArr = Enum.map(points, fn [x, _] -> x end)
     |> Enum.sort(:asc)
-
-    xArrDuplicate = xArr ++ [List.last(xArr)]
-
-    xArr
-    |> Enum.with_index
-    |> Enum.reduce([], fn ({el, i}, acc) ->
-        acc ++ [Enum.at(xArrDuplicate, i+1) - el]
-    end)
+    |> calculate_width
     |> Enum.max
   end
 end
