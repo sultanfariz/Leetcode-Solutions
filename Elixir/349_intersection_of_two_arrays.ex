33e79f04b1ed8105de6f5e2f695463a18ec5235b
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2024-03-10 12:38:24 +0700

    Update 349_intersection_of_two_arrays.ex
    Use Two-Pointer instead of HashMap

--- Elixir/349_intersection_of_two_arrays.ex
+++ Elixir/349_intersection_of_two_arrays.ex
@@ -1,23 +1,16 @@
 defmodule Solution do
+  @spec traverse_arr(nums1 :: [integer], nums2 :: [integer], result :: [integer]) :: [integer]
+  defp traverse_arr(_, [], res), do: res
+  defp traverse_arr([], _, res), do: res
+  defp traverse_arr([a | t1], [a | t2], [a | res]), do: traverse_arr(t1, t2, [a | res])
+  defp traverse_arr([a | t1], [a | t2], res), do: traverse_arr(t1, t2, [a | res])
+  defp traverse_arr([a | t1], [b | t2], res) when a > b, do: traverse_arr([a | t1], t2, res)
+  defp traverse_arr([a | t1], [b | t2], res) when a < b, do: traverse_arr(t1, [b | t2], res)
+
   @spec intersection(nums1 :: [integer], nums2 :: [integer]) :: [integer]
   def intersection(nums1, nums2) do
-    maps1 = Enum.reduce(nums1, %{}, fn (el, acc) -> 
-      Map.update(acc, el, 1, fn acc -> acc end)
-    end)
-    maps2 = Enum.reduce(nums2, %{}, fn (el, acc) -> 
-      Map.update(acc, el, 1, fn acc -> acc end)
-    end)
-
-    Enum.reduce(nums1, [], fn (el, acc) ->
-      a = Map.fetch(maps1, el)
-      b = Map.fetch(maps2, el)
-      if a == b do
-        acc ++ [el]
-      else
-        acc
-      end
-    end)
-    |> Enum.frequencies
-    |> Enum.map(fn {a, b} -> a end)
+    nums1 = nums1 |> Enum.sort(&(&1 < &2))
+    nums2 = nums2 |> Enum.sort(&(&1 < &2))
+    traverse_arr(nums1, nums2, [])
   end
 end
