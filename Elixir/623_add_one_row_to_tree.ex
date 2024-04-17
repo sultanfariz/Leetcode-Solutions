a7dfc6802d46db7803151f542d080b3daaa9e901
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2024-04-17 11:26:24 +0700

    Update 623_add_one_row_to_tree.ex

--- Elixir/623_add_one_row_to_tree.ex
+++ Elixir/623_add_one_row_to_tree.ex
@@ -10,19 +10,19 @@
 # end
 
 defmodule Solution do
-  @spec add_one_row(root :: TreeNode.t | nil, currentNode :: TreeNode.t | nil, val :: integer, depth :: integer, isLeft :: boolean) :: TreeNode.t | nil
-  defp add_one_row(_, nil, val, 0, _), do: %TreeNode{val: val, left: nil, right: nil}
-  defp add_one_row(_, nil, _, _, _), do: nil
-  defp add_one_row(_, currentNode, val, 0, false), do: %TreeNode{val: val, left: nil, right: currentNode}
-  defp add_one_row(_, currentNode, val, 0, true), do: %TreeNode{val: val, left: currentNode, right: nil}
-  defp add_one_row(root, currentNode, val, depth, _) do
-    left = add_one_row(root, currentNode.left, val, depth-1, true)
-    right = add_one_row(root, currentNode.right, val, depth-1, false)
-    %TreeNode{val: currentNode.val, left: left, right: right}
+  @spec add_one_row(root :: TreeNode.t | nil, val :: integer, depth :: integer, isLeft :: boolean) :: TreeNode.t | nil
+  defp add_one_row(nil, val, 0, _), do: %TreeNode{val: val, left: nil, right: nil}
+  defp add_one_row(nil, _, _, _), do: nil
+  defp add_one_row(root, val, 0, false), do: %TreeNode{val: val, left: nil, right: root}
+  defp add_one_row(root, val, 0, true), do: %TreeNode{val: val, left: root, right: nil}
+  defp add_one_row(root, val, depth, _) do
+    left = add_one_row(root.left, val, depth-1, true)
+    right = add_one_row(root.right, val, depth-1, false)
+    %TreeNode{val: root.val, left: left, right: right}
   end
 
   @spec add_one_row(root :: TreeNode.t | nil, val :: integer, depth :: integer) :: TreeNode.t | nil
   def add_one_row(root, val, depth) do
-    add_one_row(root, root, val, depth-1, true)
+    add_one_row(root, val, depth-1, true)
   end
 end
