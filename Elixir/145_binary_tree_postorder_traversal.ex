ae0e3e79de1336bea8b2cebc274572998912f57d
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2024-08-25 09:02:50 +0700

    Create 145_binary_tree_postorder_traversal.ex

---
+++ Elixir/145_binary_tree_postorder_traversal.ex
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+#
+# defmodule TreeNode do
+#   @type t :: %__MODULE__{
+#           val: integer,
+#           left: TreeNode.t() | nil,
+#           right: TreeNode.t() | nil
+#         }
+#   defstruct val: 0, left: nil, right: nil
+# end
+
+defmodule Solution do
+  @spec postorder_traversal(root :: TreeNode.t | nil) :: [integer]
+  def postorder_traversal(nil), do: []
+  def postorder_traversal(root) do
+    postorder_traversal(root.left) ++ postorder_traversal(root.right) ++ [root.val]
+  end
+end
